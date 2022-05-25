4b30006d42ec31e5dae2e3f8d2c8cacae465c5dc
l4ki2000 <105380566+l4ki2000@users.noreply.github.com>
2022-05-25 16:33:17 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,15 +3,16 @@ from sample_config import Config
 
 class Development(Config):
 # ملاحضه تمسح الكلام العربي وتضع ببدالة الكود
-    APP_ID = 6
-    API_HASH = "الايبي هاش"
-    ALIVE_NAME = "ضع اسمك"
-    DB_URI = "ضع رابط التخزين"
-    STRING_SESSION = "ضع كود تريمكس"
-    TG_BOT_TOKEN = "ضع توكن بوتك"
-    TG_BOT_USERNAME = "ضع معرف بوتك"
+    APP_ID = 13830708
+    API_HASH = "c3ba674ca191c3c54b4283a3634ad47e"
+    ALIVE_NAME = "haider "
+    DB_URI = "postgres://ffkbikiy:U7A-ogeWkcqvJM4rt7vV6CGePK3fEoZt@fanny.db.elephantsql.com/ffkbikiy"
+    STRING_SESSION = "1ApWapzMBu6TxCK6ma_xCY2SF3LoRCqas22qSrTemuznHzT0nsmqDxSUdD0Gq9Ry12FpGCA5wTfzvc8XGUEcmZji5gneP0RtQ-PeRqyoAO-vfL0vEmJ0lySs4KszDXptAZi7v7biTTexy-hIDAdanTUGDSKmqEnqvkZ15ghbbUYrYYX7uqUZAZvhB1pp952CH8bSvmadoQeo6vVTOzCnOsXYyPC0XPUW5NtkWv34w0mmW91_7pHY84hHDsfWvVsAq3sGI8DWGvtjoGZGj9WyGdEABAvnxXQ8d78WEBMHQgmQsTl-eGAr0fNPyZ8glJZfNv051GqoOUvwqCDLyrNwGg5vyIn387SU="
+    TG_BOT_TOKEN = "5037459394:AAGMeSLo2tuv6RHovscsSmuxWb_nQYXoQDw"
+    TG_BOT_USERNAME = "eidhhdi_bot"
     PRIVATE_GROUP_BOT_API_ID = -100
     COMMAND_HAND_LER = "."
     SUDO_COMMAND_HAND_LER = "."
     TZ = "Asia/Baghdad"
 # تليثون العرب - @IQTHON
+
